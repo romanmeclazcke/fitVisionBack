306a867d8021ea414c9b016082ba3c11ecca090b
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2026-03-12 14:22:45 -0300

    feat: add logic to change password

--- src/main/java/org/example/fitvisionback/auth/dto/ChangePasswordDto.java
+++ src/main/java/org/example/fitvisionback/auth/dto/ChangePasswordDto.java
@@ -2,12 +2,11 @@ package org.example.fitvisionback.auth.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.checkerframework.checker.units.qual.A;
 
 @Data
 @NoArgsConstructor
-@A
 public class ChangePasswordDto {
     private String oldPassword;
     private String newPassword;
+    private String confirmNewPassword;
 }
